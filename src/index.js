bd4118102fb93a7e64c65b2319fac3c4d7493146
yenlx <yenlx@vnext.com.vn>
2019-01-08 15:48:03 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ import {Subject} from './noredux/index'
 import Demo from './demo/index'
 import LazyLoad from './demo/lazyLoadComponent'
 import LazyLoadImage from './demo/lazyLoadImage'
+import Throttle from './demo/throttleDemo'
+import Debounce from './demo/debounceDemo'
 
 
-ReactDOM.render(<LazyLoadImage />, document.getElementById('root'));
+
+ReactDOM.render(<Debounce />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
